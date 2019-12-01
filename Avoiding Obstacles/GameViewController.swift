89bb7324cb1ffe0d4da78f7485c77d2223c5888b
Václav Smítal <vaclav@smit.al>
2019-12-01 13:35:15 +0100

    changes

--- Avoiding Obstacles/GameViewController.swift
+++ Avoiding Obstacles/GameViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var barrierViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var playerViewLeftConstraint: NSLayoutConstraint!
+    
+    var timer: Timer?
+    var screenWidth: CGFloat = 0
+    var screenHeight: CGFloat = 0
+    var horizontalPosition: CGFloat = 128
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let screenSize = UIScreen.main.bounds
+        self.screenWidth = screenSize.width
+        self.screenHeight = screenSize.height
+        self.playerView.layer.cornerRadius = 24
+        playerViewLeftConstraint.constant = self.screenWidth / 2
+        self.timer = Timer.scheduledTimer(timeInterval: 0.025, target: self, selector: #selector(fire), userInfo: nil, repeats: true)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+
+    @objc func fire()
+    {
+        self.barrierViewTopConstraint.constant += 1
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -20,7 +48,15 @@ class GameViewController: UIViewController {
 
         if let touch = touches.first {
             let location = touch.location(in: self.view)
-            print(location.x)
+            if(location.x < 24) {
+                self.horizontalPosition = 24
+            } else if(location.x > (self.screenWidth - 24)) {
+                self.horizontalPosition = self.screenWidth - 24
+            } else {
+                self.horizontalPosition = location.x
+            }
+
+            playerViewLeftConstraint.constant = self.horizontalPosition
         }
     }
 
--- Avoiding Obstacles/ViewController.swift
+++ Avoiding Obstacles/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
