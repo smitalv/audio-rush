4c31713c64fc04a796bfafba24af0b620045ffed
Václav Smítal <vaclav@smit.al>
2019-12-01 10:13:33 +0100

    Initial Commit

---
+++ Avoiding Obstacles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Avoiding Obstacles
+//
+//  Created by Václav Smítal on 01/12/2019.
+//  Copyright © 2019 Václav Smítal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
