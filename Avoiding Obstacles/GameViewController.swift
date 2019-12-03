91203e3a51c7a185258ce33e0298c0356e609460
Václav Smítal <vaclav@smit.al>
2019-12-03 22:34:13 +0100

    changes

--- Avoiding Obstacles/GameViewController.swift
+++ Avoiding Obstacles/GameViewController.swift
@@ -21,16 +21,18 @@ class GameViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     
     var timer: Timer?
+    var beepTimer: Timer?
     var screenWidth: CGFloat = 0
     var screenHeight: CGFloat = 0
     var horizontalPosition: CGFloat = 0
+    var beepPlayer: AVAudioPlayer?
     var player: AVAudioPlayer?
-    var count = 100
     var holePosition: CGFloat = 128
     var step: CGFloat = 1
     var steps = 0
     var score = 0
     var holeWidth: CGFloat = 128
+    var status = "ok"
     
     override var prefersStatusBarHidden: Bool {
         return true
@@ -54,6 +56,7 @@ class GameViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         self.timer = Timer.scheduledTimer(timeInterval: 0.005, target: self, selector: #selector(fire), userInfo: nil, repeats: false)
+        self.beepTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(beep), userInfo: nil, repeats: false)
     }
 
     @objc func fire()
@@ -67,20 +70,6 @@ class GameViewController: UIViewController {
                 self.scoreLabel.text = String(self.score)
             }
         }
-
-        self.count += 1
-        if(self.count >= 100 && self.playerView.frame.origin.y + 24 >= self.barrierView.frame.origin.y - 16) {
-            var soundName: String
-            if(self.horizontalPosition - 24 < self.holePosition - (self.holeWidth / 2)) {
-                soundName = "a4"
-                self.playSound(soundName: soundName)
-            } else if(self.horizontalPosition + 24 > self.holePosition + (self.holeWidth / 2)) {
-                soundName = "d4"
-                self.playSound(soundName: soundName)
-            }
-            
-            self.count = 0
-        }
         
         self.steps += 1
         if(self.steps >= 1000) {
@@ -88,7 +77,12 @@ class GameViewController: UIViewController {
             self.barrierView.alpha -= 1/3
             self.holePosition = CGFloat.random(in: 64 ... (self.screenWidth - 64))
             self.holeViewLeftConstraint.constant = self.holePosition;
-            
+
+            if(self.score > 20 && self.holeWidth > 64) {
+                self.holeWidth = CGFloat(128 - 0.5 * Double(self.score - 20))
+                self.holeViewWidthConstraint.constant = self.holeWidth
+            }
+
             self.steps = 0
         }
         
@@ -98,7 +92,28 @@ class GameViewController: UIViewController {
         if(interval < 0.003) {
             interval = 0.003
         }
+
         self.timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: #selector(fire), userInfo: nil, repeats: false)
+    }
+    
+    func playBeep(soundName: String) {
+
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            beepPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            beepPlayer?.rate = 2
+
+            guard let beepPlayer = beepPlayer else { return }
+
+            beepPlayer.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 
     func playSound(soundName: String) {
@@ -121,10 +136,37 @@ class GameViewController: UIViewController {
         }
     }
 
+    @objc func beep()
+    {
+        if(self.playerView.frame.origin.y + 24 >= self.barrierView.frame.origin.y - 16) {
+            if(self.horizontalPosition - 24 < self.holePosition - (self.holeWidth / 2)) {
+                self.status = "left"
+            } else if(self.horizontalPosition + 24 > self.holePosition + (self.holeWidth / 2)) {
+                self.status = "right"
+            } else {
+                self.status = "ok"
+            }
+        } else {
+            self.status = "ok"
+        }
+
+        var soundName: String
+        if(self.status == "left") {
+            soundName = "a4"
+            self.playBeep(soundName: soundName)
+            self.beepTimer = Timer.scheduledTimer(timeInterval: TimeInterval(0.25 - (self.horizontalPosition - self.holePosition) * 0.001), target: self, selector: #selector(beep), userInfo: nil, repeats: false)
+        } else if(self.status == "right") {
+            soundName = "d4"
+            self.playBeep(soundName: soundName)
+            self.beepTimer = Timer.scheduledTimer(timeInterval: TimeInterval(0.25 - (self.holePosition - self.horizontalPosition) * 0.001), target: self, selector: #selector(beep), userInfo: nil, repeats: false)
+        } else {
+            self.beepTimer = Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(beep), userInfo: nil, repeats: false)
+        }
+    }
+
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
-
-
+        
         if let touch = touches.first {
             let location = touch.location(in: self.view)
             if(location.x < 24) {
@@ -136,12 +178,16 @@ class GameViewController: UIViewController {
             }
 
             playerViewLeftConstraint.constant = self.horizontalPosition
+
         }
+        
     }
     
     @IBAction func tappedPause(_ sender: UIButton) {
         self.timer?.invalidate()
         self.timer = nil
+        self.beepTimer?.invalidate()
+        self.beepTimer = nil
         self.dismiss(animated: false)
     }
     
